b952da82036bea4abc2b32bb722d506cfc69d2a7
MauricioGedrat <mauriciogedrat@gmail.com>
2022-12-15 11:19:03 -0300

    responsive contact form

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -5,7 +5,7 @@ export function ContactForm() {
   const [state, handleSubmit] = useForm("xoqzaaab");
 
   return (
-    <div className="flex justify-center items-center flex-col w-[1200px] m-auto">
+    <div className="flex justify-center items-center flex-col max-w-[1200px] m-auto">
       <div className="mt-32 flex justify-center mb-10" id="projects">
         <h1
           data-aos="zoom-in-left"
@@ -29,7 +29,7 @@ export function ContactForm() {
             id="name"
             type="text"
             name="name"
-            className="w-[1200px] rounded-md mb-5 p-4 bg-gray-900 text-white"
+            className="xl:w-[1200px] lg:w-[800px] md:w-[600px] sm:w-[400px] w-[300px] m-auto rounded-md mb-5 p-4 bg-gray-900 text-white"
           />
         </div>
         <ValidationError prefix="Name" field="name" errors={state.errors} />
@@ -47,7 +47,7 @@ export function ContactForm() {
             id="email"
             type="email"
             name="email"
-            className="w-[1200px] rounded-md mb-5 p-4 bg-gray-900 text-white"
+            className="xl:w-[1200px] lg:w-[800px] md:w-[600px] sm:w-[400px] w-[300px] m-auto rounded-md mb-5 p-4 bg-gray-900 text-white"
           />
         </div>
         <ValidationError prefix="Email" field="email" errors={state.errors} />
@@ -63,7 +63,7 @@ export function ContactForm() {
           <textarea
             id="message"
             name="message"
-            className="w-[1200px] rounded-md mb-5 p-4 bg-gray-900 text-white"
+            className="xl:w-[1200px] lg:w-[800px] md:w-[600px] sm:w-[400px] w-[300px] m-auto rounded-md mb-5 p-4 bg-gray-900 text-white"
           />
         </div>
 
@@ -72,20 +72,22 @@ export function ContactForm() {
           field="message"
           errors={state.errors}
         />
-        <div className="flex gap-[20px]">
+        <div className="md:flex gap-[20px]">
           <button
             type="submit"
-            className="form-btn border-4 border-blue-500 font-bold text-blue-500 rounded-lg text-2xl hover:bg-blue-500 hover:text-white duration-300 px-20 py-4 "
+            className="w-full md:w-auto form-btn mb-5 border-4 border-blue-500 font-bold text-blue-500 rounded-lg text-2xl hover:bg-blue-500 hover:text-white duration-300 px-20 py-4 "
             disabled={state.submitting}
           >
             Send
           </button>
-          <a
-            className="flex justify-center gap-2 items-center whats-btn border-4 border-[#25D366] hover:bg-[#25D366] font-bold rounded-lg text-white text-xl duration-300 text-center px-4 py-4"
-            href="https://api.whatsapp.com/send/?phone=5551997967855"
-          >
-            Whatsapp <WhatsSvg/>
-          </a>
+          <div>
+            <a
+              className="flex justify-center gap-2 items-center whats-btn border-4 border-[#25D366] hover:bg-[#25D366] font-bold rounded-lg text-white text-xl duration-300 text-center px-4 py-4"
+              href="https://api.whatsapp.com/send/?phone=5551997967855"
+            >
+              Whatsapp <WhatsSvg />
+            </a>
+          </div>
         </div>
       </form>
     </div>
