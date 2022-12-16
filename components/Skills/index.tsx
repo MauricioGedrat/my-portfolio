daae43c8ab9a7ee210a8d3391ff48c40f13c9dcd
MauricioGedrat <mauriciogedrat@gmail.com>
2022-12-16 13:39:59 -0300

    Skills alterations

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -23,68 +23,100 @@ export const Skills = () => {
 
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-20">
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <ReactSvg />
 
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-l-full h-full flex justify-center bg-blue-500  w-[70%]">
-                React.js: 70%
+            <div className="relative  flex items-center text-white ">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">React.js:</span> 1 year of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <NextSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-l-full h-full flex justify-center bg-blue-500  w-[75%]">
-                Next.js: 75%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Next.js:</span> 10 months of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <TailwindSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-full h-full flex justify-center bg-blue-500  w-[100%]">
-                Tailwind CSS: 100%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Tailwind CSS:</span> 1 year of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <ViteSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-l-full h-full flex justify-center bg-blue-500  w-[80%]">
-                Vite.js: 80%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Vite.js:</span> 7 months of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <NodeSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-sm  sm:text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-l-full h-full flex items-center justify-center bg-blue-500  w-[40%]">
-                Node.js: 20%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Node.js:</span> Without much experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <GitSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-l-full h-full flex justify-center bg-blue-500  w-[85%]">
-                Git: 85%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Git:</span> 1 year and 3 months of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <GitHubSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-full h-full flex justify-center bg-blue-500  w-[100%]">
-                GitHub: 100%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">GitHub:</span>1 year and 5 months of experience
               </div>
             </div>
           </div>
-          <div data-aos='flip-down' data-aos-duration={2000} className="md:flex-row flex-col flex justify-center items-center gap-14">
+          <div
+            data-aos="flip-down"
+            data-aos-duration={2000}
+            className="md:flex-row flex-col flex justify-center items-center gap-14"
+          >
             <VsCodeSvg />
-            <div className="relative  flex items-center md:w-96 w-60 text-xl border-4 rounded-full h-10 border-blue-500 text-white ">
-              <div className="absolute left-0 top-0 rounded-full h-full flex justify-center bg-blue-500  w-[100%]">
-                Visual Studio Code: 100%
+            <div className="relative flex items-center text-white">
+              <div className=" w-[250px] font-['Poppins'] text-[#d3d3d3] text-center text-xl">
+                <span className="font-bold text-white">Visual Studio Code:</span> 2 years of experience
               </div>
             </div>
           </div>
